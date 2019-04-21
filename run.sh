972d66f315110ae5da5b5cd276c9d6307a2b627f
Benjamin Vezon <ben.amuerte@gmail.com>
2019-04-21 12:42:45 +0200

    feat: add env var to run.sh

--- run.sh
+++ run.sh
@@ -3,7 +3,8 @@
 JAVA_DEBUG_OPTS="-Xrunjdwp:transport=dt_socket,address=8000,server=y,suspend=n"
 JAVA_OPTS="${JAVA_DEBUG_OPTS}"
 
-gradle clean build shadowJar
 
-echo java "${JAVA_OPTS}" -jar build/libs/borbah-all.jar server build/resources/main/configuration.yml
+export AWS_ACCESS_KEY_ID="keyId"
+export AWS_SECRET_ACCESS_KEY="secret"
+
 java "${JAVA_OPTS}" -jar build/libs/borbah-all.jar server build/resources/main/configuration.yml
