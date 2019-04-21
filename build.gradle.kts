caf4f9235ee80bac34d0720282c18da8cb812b2f
Benjamin Vezon <ben.amuerte@gmail.com>
2019-04-21 12:17:40 +0200

    feat: add swagger2markup

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+import io.github.swagger2markup.tasks.Swagger2MarkupTask
 import org.jetbrains.dokka.gradle.DokkaTask
 import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-import org.openapitools.generator.gradle.plugin.tasks.GenerateTask
 
 val group = "org.amuerte.gaming"
 val version = "1.0.0-SNAPSHOT"
@@ -10,14 +10,13 @@ val version = "1.0.0-SNAPSHOT"
 plugins {
     val kotlinVersion = "1.3.21"
     val shadowJarVersion = "5.0.0"
-    val openApiPluginVersion = "4.0.0-beta3"
     val dokkaVersion="0.9.18"
 
     kotlin("jvm") version kotlinVersion
     kotlin("kapt") version kotlinVersion
     id("com.github.johnrengelman.shadow") version shadowJarVersion
-    id("org.openapi.generator") version openApiPluginVersion
     id("org.jetbrains.dokka") version dokkaVersion
+    id("io.github.lhotari.swagger2markup") version "1.3.3.1"
 }
 
 repositories {
@@ -59,30 +58,17 @@ tasks.withType<Jar> {
     manifest.attributes.put("Main-Class", "org.amuerte.gaming.BorbahKt")
 }
 
+tasks.withType<Swagger2MarkupTask> {
+    swaggerInput = "${project.rootDir}/src/main/resources/api/players-api-swagger2.yml"
+    outputDir = file("${project.buildDir}/docs/api/")
+}
+
 val test by tasks.getting(Test::class) {
     useJUnitPlatform {
         exclude("**/**TestIT.class")
     }
 }
 
-tasks.withType<GenerateTask> {
-    generateApiDocumentation.set(true)
-    generateModelDocumentation.set(true)
-    inputSpec.set("$rootDir/src/main/resources/api/players-api.yml")
-    apiPackage.set("org.amuerte.gaming.api")
-    invokerPackage.set("org.amuerte.gaming.invoker")
-    modelPackage.set("org.amuerte.gaming.model")
-}
-
 tasks.withType<DokkaTask> {
     outputFormat = "gfm"
-}
-
-tasks.register<GenerateTask>("generationDoc") {
-    generatorName.set("html2")
-    outputDir.set("$buildDir/generated/openapi/doc")
-}
-
-openApiValidate() {
-    inputSpec.set("$rootDir/src/main/resources/api/players-api.yml")
 }
